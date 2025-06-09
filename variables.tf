ef8e452ea2de749cee000969e97ff1dd7977b397
AntOsk73 <antoine.osky@bnc.ca>
2025-06-09 19:53:17 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,31 +5,6 @@ variable "AWS_CLIENT_SECRET"{
   type=string
 }
 
-variable "bucket_names" {
-  type    = list(string)
-  default = ["Noninna_bucket-1", "900_bucket-2", "bordel_bucket-3", "eaton_bucket-4", "stjacques_bucket-5"]
-}
-
-variable "instance_count" {
-  default = 5
-}
-
-variable "ami_id" {
-  description = "Amazon Machine Image ID"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "key_name" {
-  description = "Key pair name"
-}
-
-variable "security_group_name" {
-  default = "terraform-sg"
-}
-
 
 
 
