47475dbbac02d37002defc8299339282e69f15ec
AntOsk73 <antoine.osky@bnc.ca>
2025-06-09 19:36:24 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,15 @@
 output "bucket_name" {
   value = aws_s3_bucket.my_bucket.bucket
 }
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
+output "private_key_pem" {
+  value     = tls_private_key.ec2_key.private_key_pem
+  sensitive = true
+}
+
+output "public_ip" {
+  value = aws_instance.mcitsami.public_ip
+}
