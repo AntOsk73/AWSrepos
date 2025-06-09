81bc47380aaa25167237241499b8a9adc3e428d3
AntOsk73 <antoine.osky@bnc.ca>
2025-06-09 19:42:06 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,9 +2,6 @@ output "bucket_name" {
   value = aws_s3_bucket.my_bucket.bucket
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.my_bucket.bucket
-}
 output "private_key_pem" {
   value     = tls_private_key.ec2_key.private_key_pem
   sensitive = true
