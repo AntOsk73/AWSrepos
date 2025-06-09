8d793a2a07899b48aa26ca601c742770c22da934
AntOsk73 <antoine.osky@bnc.ca>
2025-06-09 19:30:35 -0400

    Update EC2.tf

--- EC2.tf
+++ EC2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2_instances" {
   count         = var.instance_count
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
+  key_name      = aws_key_pair.generated.key_name
 
   tags = {
     Name = "Terraform-Instance-${count.index}"
