8bc8e24bff38049c6b3bd4043b03ebc83048fbca
AntOsk73 <antoine.osky@bnc.ca>
2025-06-09 19:55:02 -0400

    Update buketloop.tf

--- buketloop.tf
+++ buketloop.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "buckets" {
+/*resource "aws_s3_bucket" "buckets" {
   for_each = toset(var.bucket_names)
 
   bucket = each.value
@@ -7,4 +7,4 @@ resource "aws_s3_bucket" "buckets" {
     Name        = each.value
     Environment = "Dev"
   }
-}
+}*/
