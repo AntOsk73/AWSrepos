69f7597bba468e896606373e89b1d15cdaa1de29
AntOsk73 <antoine.osky@bnc.ca>
2025-06-11 18:14:51 -0400

    Update 110625variables.tf

--- 110625variables.tf
+++ 110625variables.tf
@@ -1,1 +1,29 @@
+variable "cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+  default     = "my-ecs-cluster"
+}
 
+variable "service_name" {
+  description = "Name of the ECS service"
+  type        = string
+  default     = "my-ecs-service"
+}
+
+variable "task_family" {
+  description = "Task definition family"
+  type        = string
+  default     = "my-task"
+}
+
+variable "image_url" {
+  description = "Docker image URL for the task"
+  type        = string
+  default     = "nginx:latest"
+}
+
+variable "container_port" {
+  description = "Port exposed by the container"
+  type        = number
+  default     = 80
+}
