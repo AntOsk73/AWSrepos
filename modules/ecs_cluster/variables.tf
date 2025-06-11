88b3a1f4cab0c5fbe62618fdddb06d61259cadf1
AntOsk73 <antoine.osky@bnc.ca>
2025-06-11 18:57:10 -0400

    Update variables.tf

--- modules/ecs_cluster/variables.tf
+++ modules/ecs_cluster/variables.tf
@@ -1,1 +1,9 @@
+variable "cluster_name" {
+  description = "Name of the ECS cluster"
+  type        = string
+}
 
+variable "capacity_provider" {
+  description = "Capacity provider for ECS"
+  type        = string
+}
