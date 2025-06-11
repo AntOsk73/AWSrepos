5525e8b6b49e9a8d03195628ca619faf68773533
AntOsk73 <antoine.osky@bnc.ca>
2025-06-11 18:15:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = var.cluster_name
+}
 
+resource "aws_ecs_task_definition" "task_def" {
+  family                   = var.task_family
+  container_definitions    = jsonencode([
+    {
+      name      = "app-container"
+      image     = var.image_url
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "ecs_service" {
+  name            = var.service_name
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.task_def.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+}
