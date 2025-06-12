9e1b190b664e0722dd8dd5e6b9989397fa9da601
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 16:25:50 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "aws_region_used" {
   description = "The AWS region in use"
   value       = var.aws_region
 }
+
+output "instance_id" {
+  description = "ID of the created EC2 instance"
+  value       = aws_instance.my_ec2.id
+}
