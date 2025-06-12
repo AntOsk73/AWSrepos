8311172a820d34e7510931bb71226d53c129b619
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 17:26:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+/*provider "aws" {
   region = var.aws_region 
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_CLIENT_SECRET
@@ -25,6 +25,7 @@ resource "aws_security_group" "my_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+*/
 
 /*Launch EC2 Instance with Key Pair & Security Group
 resource "aws_instance" "my_ec2" {
@@ -38,7 +39,7 @@ resource "aws_instance" "my_ec2" {
   }
 }*/
 
-# Launch Multiple EC2 Instances Using `for_each` (Without SSH Key)
+/*# Launch Multiple EC2 Instances Using `for_each` (Without SSH Key)
 resource "aws_instance" "my_ec2" {
   for_each       = var.instance_names
   ami            = "ami-12345678"
@@ -48,6 +49,6 @@ resource "aws_instance" "my_ec2" {
   tags = {
     Name = each.key
   }
-}
+}*/
 
 
