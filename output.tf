a71b30cebc651cfd208e924fd4b2dbe5a420f2e7
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 17:11:09 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -17,3 +17,13 @@ output "security_group_id" {
   description = "ID of the security group"
   value       = aws_security_group.my_sg.id
 }
+
+output "instance_ids" {
+  description = "IDs of created EC2 instances"
+  value       = { for name, instance in aws_instance.my_ec2 : name => instance.id }
+}
+
+output "instance_public_ips" {
+  description = "Public IPs of created EC2 instances"
+  value       = { for name, instance in aws_instance.my_ec2 : name => instance.public_ip }
+}
