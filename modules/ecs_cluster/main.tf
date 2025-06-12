67812317e8e2d8fc194758358d809bd1dbe76500
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 15:52:34 -0400

    Update main.tf

--- modules/ecs_cluster/main.tf
+++ modules/ecs_cluster/main.tf
@@ -1,9 +1,1 @@
-resource "aws_ecs_cluster" "ecs" {
-  name = var.cluster_name
-}
 
-resource "aws_ecs_capacity_provider" "ecs_provider" {
-  name             = var.cluster_name
-  cluster_name     = aws_ecs_cluster.ecs.name
-  capacity_provider = var.capacity_provider
-}
