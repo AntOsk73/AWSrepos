e7c08557aeb352026e735bbfbd9ffa83f36ce92c
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 16:23:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ provider "aws" {
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_CLIENT_SECRET
 }
+
+resource "aws_instance" "my_ec2" {
+  ami           = "ami-12345678" # Replace with a valid AMI ID
+  instance_type = var.instance_type # Uses the variable from variables.tf
+
+  tags = {
+    Name = "MyTerraformInstance"
+  }
+}
