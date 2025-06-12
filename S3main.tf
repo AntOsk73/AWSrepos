f0bddedd1f052c02969646657d8259cdd7957840
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 17:26:30 -0400

    Update S3main.tf

--- S3main.tf
+++ S3main.tf
@@ -1,1 +1,16 @@
+provider "aws" {
+  region = var.region
+}
 
+resource "aws_s3_bucket" "my_bucket" {
+  bucket        = var.bucket_name
+  force_destroy = true # Allows deletion of the bucket even if it has objects
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket                  = aws_s3_bucket.my_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
