917bb70912ac5ddd2854c1d689abb0b2dcdb99c3
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 16:50:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,6 @@ provider "aws" {
   secret_key=var.AWS_CLIENT_SECRET
 }
 
-resource "aws_instance" "my_ec2" {
-  ami           = "ami-12345678" # Replace with a valid AMI ID
-  instance_type = var.instance_type # Uses the variable from variables.tf
-
-  tags = {
-    Name = "MyTerraformInstance"
-  }
-}
-
 # Create the EC2 Key Pair
 resource "aws_key_pair" "my_key" {
   key_name   = var.key_name
@@ -44,7 +35,7 @@ resource "aws_security_group" "my_sg" {
 # Launch EC2 Instance with Key Pair & Security Group
 resource "aws_instance" "my_ec2" {
   ami             = "ami-12345678"  # Replace with a valid AMI ID
-  instance_type   = "t2.micro"
+  instance_type   = var.instance_type # Uses the variable from variables.tf
   key_name        = aws_key_pair.my_key.key_name  # Attach SSH key
   security_groups = [aws_security_group.my_sg.name]  # Assign security group
 
