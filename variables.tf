005c1218277efeedbb1d376c112632fa76e43691
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 16:00:12 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,14 @@
 variable "AWS_ACCESS_KEY"{
   type=string
 }
-variable "AWS_CLIENT_SECRET"{
+variable "AWS_SECRET_KEY"{
   type=string
+}
+
+variable "aws_region" {
+  description = "Where do you want to build your AWS stuff?"
+  type        = string
+  default     = "us-east-1"  # This means if you don't pick, it'll go here.
 }
 
 
