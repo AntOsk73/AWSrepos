b49adf4b6340c1ad2931f538b2b81d3f498a97b4
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 17:32:31 -0400

    Update S3main.tf

--- S3main.tf
+++ S3main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = var.region
-}
-
 resource "aws_s3_bucket" "my_bucket" {
   bucket        = var.bucket_name
   force_destroy = true # Allows deletion of the bucket even if it has objects
