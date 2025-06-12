771cb884829f01a509c98ba07ae62f53508daa8e
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 17:38:04 -0400

    Update S3.outputs.tf

--- S3.outputs.tf
+++ S3.outputs.tf
@@ -1,4 +1,4 @@
-output "bucket_id" {
+/*output "bucket_id" {
   description = "The ID of the S3 bucket"
   value       = aws_s3_bucket.my_bucket.id
 }
@@ -6,4 +6,14 @@ output "bucket_id" {
 output "bucket_arn" {
   description = "The ARN of the S3 bucket"
   value       = aws_s3_bucket.my_bucket.arn
+}*/
+
+output "bucket_ids" {
+  description = "The IDs of the S3 buckets"
+  value       = { for k, v in aws_s3_bucket.buckets : k => v.id }
+}
+
+output "bucket_arns" {
+  description = "The ARNs of the S3 buckets"
+  value       = { for k, v in aws_s3_bucket.buckets : k => v.arn }
 }
