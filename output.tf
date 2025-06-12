fda87458aec6a6d19c4805bf642223f01c17614b
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 17:14:45 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,14 +6,14 @@ output "aws_region_used" {
 /*output "instance_id" {
   description = "ID of the created EC2 instance"
   value       = aws_instance.my_ec2.id
-}
+}*/
 
-output "instance_public_ip" {
+/*output "instance_public_ip" {
   description = "Public IP for SSH access"
   value       = aws_instance.my_ec2.public_ip
-}
+}*/
 
-output "security_group_id" {
+/*output "security_group_id" {
   description = "ID of the security group"
   value       = aws_security_group.my_sg.id
 }*/
