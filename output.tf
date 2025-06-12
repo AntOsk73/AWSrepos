6102e6b469c312de7ebdf161f8ed6df78aaabded
AntOsk73 <antoine.osky@bnc.ca>
2025-06-12 17:27:08 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "aws_region_used" {
+/*output "aws_region_used" {
   description = "The AWS region in use"
   value       = var.aws_region
-}
+}*/
 
 /*output "instance_id" {
   description = "ID of the created EC2 instance"
@@ -18,12 +18,12 @@ output "aws_region_used" {
   value       = aws_security_group.my_sg.id
 }*/
 
-output "instance_ids" {
+/*output "instance_ids" {
   description = "IDs of created EC2 instances"
   value       = { for name, instance in aws_instance.my_ec2 : name => instance.id }
-}
+}*/
 
-output "instance_public_ips" {
+/*output "instance_public_ips" {
   description = "Public IPs of created EC2 instances"
   value       = { for name, instance in aws_instance.my_ec2 : name => instance.public_ip }
-}
+}*/
